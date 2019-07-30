08e7946afc40633a8054a3cb86a2b8e2cb4969f2
Ramdoni <doni.enginer@gmail.com>
2019-07-30 23:40:54 +0700

    Update email config

--- application/config/email.php
+++ application/config/email.php
@@ -1,12 +1,26 @@
 <?php defined('BASEPATH') OR exit('No direct script access allowed');
 
+// $config = array(
+//     'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
+//     'smtp_host' => 'mail.pmt.co.id', 
+//     'smtp_port' => 465,
+//     'smtp_user' => 'epl@pmt.co.id',
+//     'smtp_pass' => ')$gh]I,Zrl+R',
+//     'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
+//     'mailtype' => 'text', //plaintext 'text' mails or 'html'
+//     'smtp_timeout' => '4', //in seconds
+//     'charset' => 'iso-8859-1',
+//     'wordwrap' => TRUE
+// );
+
+
 $config = array(
     'protocol' => 'smtp', // 'mail', 'sendmail', or 'smtp'
-    'smtp_host' => 'mail.pmt.co.id', 
+    'smtp_host' => 'smtp.gmail.com', 
     'smtp_port' => 587,
-    'smtp_user' => 'epl@pmt.co.id',
-    'smtp_pass' => ')$gh]I,Zrl+R',
-    'smtp_crypto' => '', //can be 'ssl' or 'tls' for example
+    'smtp_user' => 'emporeht@gmail.com',
+    'smtp_pass' => 'Empore45',
+    'smtp_crypto' => 'tls', //can be 'ssl' or 'tls' for example
     'mailtype' => 'text', //plaintext 'text' mails or 'html'
     'smtp_timeout' => '4', //in seconds
     'charset' => 'iso-8859-1',
