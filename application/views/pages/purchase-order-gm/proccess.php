ec2b007d47070347c6e679f804ba9d94e89f66fc
Doni <doni.enginer@gmail.com>
2019-07-31 15:37:10 +0700

    Update vendor

--- application/views/pages/purchase-order-gm/proccess.php
+++ application/views/pages/purchase-order-gm/proccess.php
@@ -82,15 +82,6 @@
                   </div>
                </div>
                <div class="form-group">
-                  <label class="control-label col-md-3 col-sm-3 col-xs-12" for="material_group">Discount
-                  </label>
-                  <div class="col-md-6 col-sm-6 col-xs-12">
-                     <input type="number" class="form-control" readonly value="<?=$data['discount']?>" placeholder="%" style="width: 150px; float: left; margin-right: 10px;">
-                     <input type="number" class="form-control" readonly name="<?=$data['discount_rp']?>" placeholder="Rp. " style="width: 250px; float: left; margin-right: 10px;">
-                  </div>
-               </div>
-
-               <div class="form-group">
                   <label class="control-label col-md-3 col-sm-3 col-xs-12" for="material_group">Note
                   </label>
                   <div class="col-md-6 col-sm-6 col-xs-12">
